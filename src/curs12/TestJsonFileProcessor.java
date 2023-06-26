33d3487a9902025ee869cb924e0f4f033792843b
Eliza Gutulov dorobantu <eliza.gutulov@yahoo.com>
2023-06-26 10:27:00 +0300

    curs13 intermediar

---
+++ src/curs12/TestJsonFileProcessor.java
@@ -0,0 +1,29 @@
+package curs12;
+
+public class TestJsonFileProcessor {
+
+	
+		public static void main(String[] args) {
+
+			
+			JsonFileProcesor obj =  new JsonFileProcesor();
+			obj.writeJsonFile();
+			
+			obj.readJson("name");
+			obj.readJson("listaObiecte");
+			obj.readJson("email");
+			
+			System.out.println("-----------------------");
+			obj.updateJsonFile("email", "altEmail");
+			System.out.println("Email after update:");
+			obj.readJson("email");
+			
+			obj.deleteFromJson("email");
+			
+			System.out.println("Email after delete:");
+			obj.readJson("email");
+		}
+
+		
+
+}
